f4d451bfa4ad0144a8309a4817c0f584c71f708c
Mauricio Morales <morales7mauro@gmail.com>
2022-06-22 18:28:43 -0500

    Ultimas modificaciones

--- API-HelpDesk/Controllers/TicketsController.cs
+++ API-HelpDesk/Controllers/TicketsController.cs
@@ -1,6 +1,7 @@
 ﻿using API_HelpDesk.Models;
 using Microsoft.AspNetCore.Mvc;
 using API_HelpDesk.DAL;
+using System.Net;
 
 namespace API_HelpDesk.Controllers
 {
@@ -13,15 +14,15 @@ namespace API_HelpDesk.Controllers
         IList<NewTicket> TicketList = new List<NewTicket>();
         public TicketController()
         {
-            NewTicket Ticket1 = new NewTicket(1, "Administrativa", "Lider 1", "Boyaca", "Garagoa", "Presencia de comején en la oficina", "", new DateTime(2022, 06, 05), "Pendiente");
-            NewTicket Ticket2 = new NewTicket(2, "Soporte", "Lider 2", "Bolívar", "Cartagena", "Capacitación del modulo nómina pendinete", "", new DateTime(2022, 06, 05), "Registrar");
-            NewTicket Ticket3 = new NewTicket(3, "Tecnología", "Lider 3", "Santander", "Bucaramanga", "Creación de nuevo módulo de tickets", "", new DateTime(2022, 06, 05), "Registrar");
-            NewTicket Ticket4 = new NewTicket(4, "Contabilidad", "Lider 2", "Cundinamarca", "Soacha", "Inclusión de campos en formulario de registro", "", new DateTime(2022, 06, 05), "Registrar");
+            //NewTicket Ticket1 = new NewTicket(1, "Administrativa", "Lider 1", "Boyaca", "Garagoa", "Presencia de comején en la oficina", "", new DateTime(2022, 06, 05), "Pendiente");
+            //NewTicket Ticket2 = new NewTicket(2, "Soporte", "Lider 2", "Bolívar", "Cartagena", "Capacitación del modulo nómina pendinete", "", new DateTime(2022, 06, 05), "Registrar");
+            //NewTicket Ticket3 = new NewTicket(3, "Tecnología", "Lider 3", "Santander", "Bucaramanga", "Creación de nuevo módulo de tickets", "", new DateTime(2022, 06, 05), "Registrar");
+            //NewTicket Ticket4 = new NewTicket(4, "Contabilidad", "Lider 2", "Cundinamarca", "Soacha", "Inclusión de campos en formulario de registro", "", new DateTime(2022, 06, 05), "Registrar");
 
-            TicketList.Add(Ticket1);
-            TicketList.Add(Ticket2);
-            TicketList.Add(Ticket3);
-            TicketList.Add(Ticket4);
+            //TicketList.Add(Ticket1);
+            //TicketList.Add(Ticket2);
+            //TicketList.Add(Ticket3);
+            //TicketList.Add(Ticket4);
         }
 
         [HttpGet]
@@ -36,12 +37,13 @@ namespace API_HelpDesk.Controllers
 
         [HttpPost]
 
-        public string GuardarData([FromBody] NewTicket TicketNew)
+        public HttpResponseMessage GuardarData([FromBody] NewTicket TicketNew)
         {
             //Instaciar conexión a base de datos
             TicketDAL ticketDAL = new TicketDAL();
-            string resultado = ticketDAL.CrearNuevoTicket(TicketNew);
-            return resultado;
+            string newTicket = ticketDAL.CrearNuevoTicket(TicketNew);
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.Conflict);
+            return res;
             
             //TicketList.Add(TicketNew);
             //return TicketList;
@@ -49,9 +51,14 @@ namespace API_HelpDesk.Controllers
 
         [HttpPut]
 
-        public IList<NewTicket> ActualizarData([FromBody] NewTicket TicketNew, int id)
+        public HttpResponseMessage ActualizarData([FromBody] NewTicket TicketNew, int id) //Tipo de dato respuesta HTTP
         {
-            int position;
+            //Instaciar conexión a base de datos
+            TicketDAL ticketDAL = new TicketDAL();
+            string newTicket = ticketDAL.UpdateTicket(TicketNew, id);
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.Conflict);
+            return res;
+            /*int position; 
             for (var i = 0; i < TicketList.Count; i++)
             {
                 if (TicketList[i].Id == id)
@@ -61,14 +68,19 @@ namespace API_HelpDesk.Controllers
 
                 }
             }
-            return TicketList;
+            return TicketList;*/
         }
 
         [HttpDelete]
 
-        public IList<NewTicket> DeleteData(int id)
+        public HttpResponseMessage DeleteData(int id)
         {
-            for (var i = 0; i < TicketList.Count; i++)
+            //Instaciar conexión a base de datos
+            TicketDAL ticketDAL = new TicketDAL();
+            string newTicket = ticketDAL.DeleteTicket(id);
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.Conflict);
+            return res;
+            /*for (var i = 0; i < TicketList.Count; i++)
             {
                 if (TicketList[i].Id == id)
                 {
@@ -76,7 +88,7 @@ namespace API_HelpDesk.Controllers
 
                 }
             }
-            return TicketList;
+            return TicketList;*/
         }
 
     }
--- API-HelpDesk/DAL/TicketDAL.cs
+++ API-HelpDesk/DAL/TicketDAL.cs
@@ -8,15 +8,15 @@ namespace API_HelpDesk.DAL
 {
     public class TicketDAL
     {
-
         public TicketDAL()
         {
-
+            
         }
 
         //CREACION DE NUEVOS TICKETS EN LA BASE DE DATOS
         public string CrearNuevoTicket(NewTicket TicketNew)
         {
+
        
             try
             {
@@ -33,7 +33,7 @@ namespace API_HelpDesk.DAL
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
 
-                    String sql = $"INSERT INTO [dbo].[TICKETS] ([AREA], [RESPONSABLE], [DEPARTAMENTO], [MUNICIPIO], [DESCRIPCION], [ESTADO]) VALUES('{TicketNew.Area}','{TicketNew.Responsable}','{TicketNew.Departamento}','{TicketNew.Municipio}','{TicketNew.Descripcion}','{TicketNew.Accion}');";
+                    String sql = $"INSERT INTO [dbo].[TICKETS] ([AREA], [RESPONSABLE], [DEPARTAMENTO], [MUNICIPIO], [DESCRIPCION],[SOLUCION], [ESTADO]) VALUES('{TicketNew.Area}','{TicketNew.Responsable}','{TicketNew.Departamento}','{TicketNew.Municipio}','{TicketNew.Descripcion}','{TicketNew.Solucion}','{TicketNew.Accion}');";
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
@@ -76,7 +76,7 @@ namespace API_HelpDesk.DAL
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
 
-                    String sql = String.Format("SELECT[ID], [AREA], [MUNICIPIO], [DESCRIPCION], [RESPONSABLE], [ESTADO] FROM[dbo].[TICKETS];");//String.Format es opcional
+                    String sql = String.Format("SELECT[ID], [AREA], [RESPONSABLE], [DEPARTAMENTO], [MUNICIPIO], [DESCRIPCION], [SOLUCION], [ESTADO] FROM [dbo].[TICKETS];");//String.Format() es opcional
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
@@ -88,10 +88,12 @@ namespace API_HelpDesk.DAL
                                 NewTicket unTicket = new NewTicket();
                                 unTicket.Id = reader.GetInt32(0);
                                 unTicket.Area = reader.GetString(1);
-                                unTicket.Municipio = reader.GetString(2);
-                                unTicket.Descripcion = reader.GetString(3);
-                                unTicket.Responsable = reader.GetString(4);
-                                unTicket.Accion = reader.GetString(5);
+                                unTicket.Responsable = reader.GetString(2);
+                                unTicket.Departamento = reader.GetString(3);
+                                unTicket.Municipio = reader.GetString(4);
+                                unTicket.Descripcion = reader.GetString(5);
+                                unTicket.Solucion = reader.GetString(6);
+                                unTicket.Accion = reader.GetString(7);
 
                                 ListaDeTickets.Add(unTicket);
                             }
@@ -101,9 +103,91 @@ namespace API_HelpDesk.DAL
             }
             catch (SqlException e)
             {
-               
+                throw e;
             }
             return ListaDeTickets;
+        }
+
+        //ACTUALIZACION DE TICKETS EN LA BASE DE DATOS
+        public string UpdateTicket(NewTicket TicketNew, int id)
+        {
+
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-DQ37319\\LOCALDB";
+                builder.UserID = "sa";
+                builder.Password = "123456";
+                builder.InitialCatalog = "HelpDeskDB";
+                builder.TrustServerCertificate = true;
+                builder.Encrypt = true;
+                builder.IntegratedSecurity = true;
+                builder.UserInstance = false;
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+
+                    String sql = $"UPDATE [dbo].[TICKETS] SET [AREA] = '{TicketNew.Area}', [RESPONSABLE] = '{TicketNew.Responsable}', [DEPARTAMENTO] = '{TicketNew.Departamento}', [MUNICIPIO] = '{TicketNew.Municipio}', [DESCRIPCION] = '{TicketNew.Descripcion}', [SOLUCION] = '{TicketNew.Solucion}', [ESTADO] = '{TicketNew.Accion}' WHERE [ID] = {id};";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            /*while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }*/
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                return "Falle " + e.Message;
+            }
+            return "Lo logre";
+        }
+
+        //ELIMINACIÓN DE TICKETS EN LA BASE DE DATOS
+        public string DeleteTicket( int id)
+        {
+            
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-DQ37319\\LOCALDB";
+                builder.UserID = "sa";
+                builder.Password = "123456";
+                builder.InitialCatalog = "HelpDeskDB";
+                builder.TrustServerCertificate = true;
+                builder.Encrypt = true;
+                builder.IntegratedSecurity = true;
+                builder.UserInstance = false;
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+
+                    String sql = $"DELETE [dbo].[TICKETS] WHERE ID = {id};";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            /*while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }*/
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                return "Falle " + e.Message;
+            }
+            return "Lo logre";
         }
 
 
